7564a70ddf02ffdd24431f33caf543937c1365a9
dotun <dotun.ogundare@gmail.com>
2023-08-28 15:55:14 +0200

    added slack messages to workflows and counter to PingPongController

--- backend/src/main/java/com/coedev/PingPongController.java
+++ backend/src/main/java/com/coedev/PingPongController.java
@@ -3,13 +3,16 @@ package com.coedev;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // another comment added 
+@RestController // another comment added
 public class PingPongController {
+
+    private static int COUNTER = 0;
 
     record PingPong(String result){}
 
     @GetMapping("/ping")
     public PingPong getPingPong(){
-        return new PingPong("Pong");
+        //return new PingPong("Pong: " + ++COUNTER);
+        return new PingPong("Pong: %s".formatted(++COUNTER));
     }
 }
